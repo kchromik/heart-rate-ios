3af36403ac39f2ed98d61a31c0416181a961438b
Kevin Chromik <chromik.kevin@gmail.com>
2016-07-11 09:20:02 +0200

    Initial Commit

---
+++ Heart rate monitor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Heart rate monitor
+//
+//  Created by Kevin Chromik on 11/07/16.
+//  Copyright © 2016 Kevin Chromik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
