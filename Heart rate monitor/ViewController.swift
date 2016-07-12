41c02dca76badbe3c8fd463ca4927c7df1cca020
Kevin Chromik <chromik.kevin@gmail.com>
2016-07-12 14:33:58 +0200

    Added table view

--- Heart rate monitor/ViewController.swift
+++ Heart rate monitor/ViewController.swift
@@ -10,20 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var currentHeartRateLabel: UILabel!
+    private var heartRateValues = [HeartRateSample]()
+    @IBOutlet weak var tableView: UITableView!
+    
+    private struct HeartRateSample {
+        var timeStamp: NSDate
+        var heartRateValue: UInt8
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         HeartRateManager.sharedManager.delegate = self
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
     @IBAction func searchDevicesPressed(sender: UIButton) {
         HeartRateManager.sharedManager.startScan()
@@ -37,7 +38,34 @@ extension ViewController: HeartRateManagerDelegate {
         let data = heartRate
         var values = [UInt8](count:data.length, repeatedValue:0)
         data.getBytes(&values, length:data.length)
+        
         let currentHeartRate = values[1]
-        currentHeartRateLabel.text = "Current HR: \(currentHeartRate)"
+        heartRateValues.append(HeartRateSample(timeStamp: NSDate(), heartRateValue: currentHeartRate))
+        tableView.reloadData()
+        
+        let path = NSIndexPath(forRow: tableView.numberOfRowsInSection(0) - 1, inSection: 0)
+        tableView.scrollToRowAtIndexPath(path, atScrollPosition: .Bottom, animated: true)
     }
+}
+
+extension ViewController: UITableViewDelegate {
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return heartRateValues.count
+    }
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("heartRateCell")
+        cell?.textLabel!.text = "\(indexPath.row):\t\(heartRateValues[indexPath.row].heartRateValue)\t\(heartRateValues[indexPath.row].timeStamp)"
+        return cell!
+    }
+    
 }
